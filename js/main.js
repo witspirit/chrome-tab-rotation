8529ac6eb8c4b9e4f9d980acacc04d364275a529
witspirit <witspirit@gmail.com>
2016-08-24 21:23:31 +0200

    Started experimenting with basic timing

--- js/main.js
+++ js/main.js
@@ -18,6 +18,24 @@ function load() {
     return JSON.parse(localStorage.state);
 }
 
+function onSchedule() {
+    // Still on, so reschedule
+    if (load().toggleValue) {
+        console.log("onSchedule");
+        schedule();
+    } else {
+        console.log("No action, no longer active");
+    }
+}
+
+function schedule() {
+    // Using window.setTimeout, since I want to support a high frequency.
+    // The alarms API will not schedule faster than 1 minute
+    // This might be an issue in combination with en Event page. Perhaps
+    // I will need to switch to a persistent background page again for this.
+    window.setTimeout(onSchedule, 3000);
+}
+
 function toggleRotation(tab) {
     console.log("toggleRotation...");
     var state = load();
@@ -27,6 +45,7 @@ function toggleRotation(tab) {
         window.alert("Disabling toggle (count="+state.toggleCount+")");
     } else {
         window.alert("Enabling toggle (count="+state.toggleCount+")");
+        schedule();
     }
     state.toggleValue = !state.toggleValue;
     state.toggleCount++;
